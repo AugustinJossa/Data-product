315745153716ba777a2624e19ddb4a08d663313d
Augustin Jossa <augustin.jossa@hec.edu>
2015-11-22 12:34:32 +0100

    Update Ui.R

--- Ui.R
+++ Ui.R
@@ -5,8 +5,8 @@ shinyUI(
                 # Body Mass Index
                 headerPanel("Calculate your Body mass index"),
                 sidebarPanel(
-                        numericInput('size', 'Size (in meter)', 90, min = 0, max = 3, step = 5),
-                        numericInput('weight', 'Weight (in kilograms)', 90, min = 0, max = 300, step = 5),
+                        numericInput('size', 'Size (in meter)', 0, min = 0, max = 3, step = 5),
+                        numericInput('weight', 'Weight (in kilograms)', 0, min = 0, max = 300, step = 5),
                         submitButton('Submit')
                 ),
                 
